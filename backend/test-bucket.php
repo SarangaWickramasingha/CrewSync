0086af177a01e7353319c7a9c5e527a5d8b340af
Saranga Wickramasingha <sandunsaranga2218@gmail.com>
2026-09-20 14:58:11 +0530

    4to

--- backend/test-bucket.php
+++ backend/test-bucket.php
@@ -1,14 +1,7 @@
 <?php
 require 'utils/s3.php';
 try {
-    $command = $s3->getCommand('GetObject', array(
-        'Bucket' => $bucketName,
-        'Key'    => 'uploads/file.jpg',
-    ));
-
-    // Create a temporary URL valid for 20 minutes
-    $presignedUrl = $s3->createPresignedUrl($command, '+20 minutes');
-
+    $presignedUrl = r2PhotoUrl('test-upload.txt');
     echo "Temporary URL: " . $presignedUrl;
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
